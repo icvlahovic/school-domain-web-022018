aac1b5fd5de8bad96a15f311fccbf89b41ca3bab
icvlahovic <ian.vlahovic@flatironschool.com>
2018-02-22 11:40:00 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    if roster[grade]
+      roster[grade]
+    end
+  end
+
+  def sort
+    sorted_roster = {}
+    roster.each { |grade, names| sorted_roster[grade] = names.sort }
+    sorted_roster
+  end
+
+end
